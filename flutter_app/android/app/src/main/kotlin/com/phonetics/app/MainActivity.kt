a1d304d9db17ba221b5cb172edb76b9b8235985f
harrington <harrington.ricks@live.com>
2026-01-10 14:52:01 -0500

    Remove deprecated Flutter code

--- flutter_app/android/app/src/main/kotlin/com/phonetics/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.phonetics.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_app/lib/config/api_config.dart
+++
@@ -1,4 +0,0 @@
-class ApiConfig {
-  static const String backendUrl = 'http://localhost:8000/api';
-  static const Duration timeout = Duration(seconds: 10);
-}
